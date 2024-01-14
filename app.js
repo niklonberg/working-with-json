a709c050731a018ebd503b4d7d32c372094e2319
Nikolai Lønberg <niklonberg@gmail.com>
2024-01-14 12:56:32 +0100

    add async functionality to populate, made populate more general so it can take different header/sectionFn's

--- app.js
+++ app.js
@@ -5,16 +5,37 @@ const getData = async (requestURL) => {
     const request = new Request(requestURL);
     const response = await fetch(request);
     if (!response.ok) {
-      throw new Error(`Failed to fecth data. Status: ${response.status}`);
+      throw new Error(`Failed to fetch data. Status: ${response.status}`);
     }
     const data = await response.json();
     log(data);
+    return data;
   } catch (error) {
     log(error.message);
     throw error;
   }
 };
 
-getData(
-  "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.jsn"
+const populateHeader = (obj) => {
+  const header = document.querySelector("header");
+  const h1 = document.createElement("h1");
+  h1.textContent = obj.squadName;
+  const p = document.createElement("p");
+  p.textContent = `Hometown: ${obj.homeTown}. Formed: ${obj.formed}`;
+  header.append(h1, p);
+};
+
+const populateSection = (obj) => {};
+
+const populate = async (requestURL, headerFn, sectionFn) => {
+  const data = await getData(requestURL);
+
+  headerFn(data);
+  sectionFn(data);
+};
+
+populate(
+  "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json",
+  populateHeader,
+  populateSection
 );
