43fba381ee2059a4e34d94d7239ba36561d2213b
Nikolai Lønberg <niklonberg@gmail.com>
2024-01-14 13:53:56 +0100

    edit tracking of total kittens, male/female, and ensured motherInfo always ends with 'and x.'

--- cats.js
+++ cats.js
@@ -8,15 +8,31 @@ const requestURL =
   "https://mdn.github.io/learning-area/javascript/oojs/tasks/json/sample.json";
 
 fetch(requestURL)
-  .then((response) => response.text())
+  .then((response) => {
+    return response.text();
+  })
   .then((text) => displayCatInfo(text));
 
 function displayCatInfo(catString) {
-  let total = 0;
+  console.log(catString);
+  let totalKittens = 0;
   let male = 0;
+  let female = 0;
 
   // Add your code here
+  const cats = JSON.parse(catString);
+  cats.forEach((cat, index, array) => {
+    console.log(cat);
+    totalKittens += cat.kittens.length;
+    cat.kittens.forEach((kitten) => {
+      kitten.gender === "m" ? (male += 1) : (female += 1);
+    });
+    index === array.length - 1
+      ? (motherInfo += `and ${cat.name}.`)
+      : (motherInfo += `${cat.name}, `);
+  });
 
+  kittenInfo = `Total kittens: ${totalKittens}, male: ${male}, female: ${female}`;
   // Don't edit the code below here!
 
   para1.textContent = motherInfo;
