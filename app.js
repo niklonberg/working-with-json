8ad2ed772be24cfc754bc1531b08fee7c2c4a30f
Nikolai Lønberg <niklonberg@gmail.com>
2024-01-14 11:10:45 +0100

    add try catch to async getData

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 log = console.log;
 
-const populate = async () => {
-  const requestURL =
-    "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
-  const request = new Request(requestURL);
-  log(request);
+const getData = async (requestURL) => {
+  try {
+    const request = new Request(requestURL);
+    const response = await fetch(request);
+    if (!response.ok) {
+      throw new Error(`Failed to fecth data. Status: ${response.status}`);
+    }
+    const data = await response.json();
+    log(data);
+  } catch (error) {
+    log(error.message);
+    throw error;
+  }
 };
 
-populate();
+getData(
+  "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.jsn"
+);
